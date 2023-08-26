38e3425e3dd9cb17f1ee627c37668c59efe20e34
rjsurys9012 <er.rjsury26574@gmail.com>
2023-08-26 17:20:02 +0530

    first commit

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import Login from "../features/auth/Component/Login";
+
+function LoginPage() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default LoginPage;
---
+++ src/pages/SignupPage.js
@@ -0,0 +1,11 @@
+import Signup from "../features/auth/Component/Signup";
+
+function SignupPage() {
+  return (
+    <>
+      <Signup />
+    </>
+  );
+}
+
+export default SignupPage;
